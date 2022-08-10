9f1084b7c81cb3a5407a4e2ea6784b755be4c00f
Daniel Jorge Csich <67213069+danielcshn@users.noreply.github.com>
2022-08-10 01:05:42 -0300

    Update pornhub_dl.py
    - Remove maximize button.
    - Centering window.
    - Change colors.
    - Correction when showing logs.

--- pornhub_dl.py
+++ pornhub_dl.py
@@ -2,7 +2,7 @@
 Python 3.9 программа скачивания pornhub
 Название файла pornhub_bot.py
 
-Version: 0.1
+Version: 0.2
 Author: Andrej Marinchenko
 Date: 2022-04-19
 """
@@ -62,54 +62,77 @@ def Download_video():
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download([url])
 
+
 class PrintLogger(): # create file like object
     def __init__(self, textbox): # pass reference to text widget
         self.textbox = textbox # keep ref
 
     def write(self, text):
+        text = text + '\n'
         self.textbox.insert(tk.END, text) # write text to textbox
+        self.textbox.see(tk.END)
+        root.update()
             # could also scroll to end of textbox here to make sure always visible
 
     def flush(self):  # needed for file like object
         pass
 
 
-# url = 'https://rt.pornhub.com/view_video.php?viewkey=ph60c0bd38c9329'
-root = tk.Tk()
-root.title('Pornhub Downloader')  # титул окна
-root.geometry('800x700')
-root.maxsize(800, 700)
-root.minsize(800, 700)
+if __name__ == '__main__':
+    # PornHub Color Palette
+    background_black = '#000000'
+    title_black = '#1b1b1b'
+    button_orange = '#ff9000'
+    text_color = '#ffffff'
 
-header = Label(root, bg="orange", width=300, height=2)
-header.place(x=0, y=0)
+    HEIGHT = 700
+    WIDTH = 800
 
-h1 = Label(root, text="Pornhub", bg="orange", fg="black", font=('verdana', 13, 'bold'))  # подпись окна вверху
-h1.place(x=135, y=5)
+    # url = 'https://rt.pornhub.com/view_video.php?viewkey=ph60c0bd38c9329'
+    root = tk.Tk()
+    root.title('Pornhub Downloader')  # титул окна
+    root.geometry('800x700')
+    root.maxsize(WIDTH, HEIGHT)
+    root.minsize(WIDTH, HEIGHT)
+    root.resizable(0,0) # this removes the maximize button
+    root.configure(bg=background_black) #set window color
 
-img = ImageTk.PhotoImage(Image.open('pornhub.png'))  # вставляем картинку logo
-logo = Label(root, image=img, borderwidth=0)
-logo.place(x=150, y=38)
-
-# определяем первое поле URL для скачивания
-e = Label(root, text="URL Address", font=('verdana', 10, 'bold'))  # надпись над полем ввода
-e.place(x=150, y=170)
-url_dl = Entry(root, width=60, relief=RIDGE, borderwidth=3)
-url_dl.place(x=150, y=190)
-
-
-
-# кнопка скачать
-download = Button(root, text="Download", padx=30, bg="orange", relief=RIDGE, borderwidth=1,
-               font=('verdana', 10, 'bold'),
-               cursor="hand2", command=Download_video)
-download.place(x=150, y=240)
-
-# Окно для вывода результата скачивания
-Output = Text(root, height = 22, width = 97)
-Output.place(x=10, y=290)
-pl = PrintLogger(Output)
-sys.stdout = pl
-
-
-root.mainloop()
+    w = root.winfo_width()
+    h = root.winfo_height()
+    ws = root.winfo_screenwidth()
+    hs = root.winfo_screenheight()
+    x = (ws/2) - (w/2)
+    y = (hs/2) - (h/2)
+    root.geometry('%dx%d+%d+%d' % (w, h, x, y)) # Centering window
+    
+    header = Label(root, bg=button_orange, width=300, height=2)
+    header.place(x=0, y=0)
+    
+    h1 = Label(root, text="Pornhub", bg=button_orange, fg=background_black, font=('arial', 13, 'bold'))  # подпись окна вверху
+    h1.place(x=WIDTH/2, y=18, anchor=CENTER)
+    
+    img = ImageTk.PhotoImage(Image.open('pornhub.png'))  # вставляем картинку logo
+    logo = Label(root, image=img, borderwidth=0)
+    logo.place(x=WIDTH/2, y=120, anchor=CENTER)
+    
+    # определяем первое поле URL для скачивания
+    e = Label(root, text="URL Address", font=('arial', 10, 'bold'), bg=background_black, fg=text_color)  # надпись над полем ввода
+    e.place(x=WIDTH/2, y=190, anchor=CENTER)
+    url_dl = Entry(root, width=60, relief=RIDGE, borderwidth=3)
+    url_dl.place(x=WIDTH/2, y=220, anchor=CENTER)
+    url_dl.focus_set()
+    
+    # кнопка скачать
+    download = Button(root, text="Download", padx=30, bg=button_orange, relief=RIDGE, borderwidth=1,
+                   font=('arial', 10, 'bold'),
+                   cursor="hand2", command=Download_video)
+    download.place(x=WIDTH/2, y=260, anchor=CENTER)
+    
+    # Окно для вывода результата скачивания
+    Output = Text(root, height = 22, width = 97)
+    Output.place(x=10, y=290)
+    Output.configure(bg=title_black, fg=text_color)
+    pl = PrintLogger(Output)
+    sys.stdout = pl
+    
+    root.mainloop()
